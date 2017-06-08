08a275ba528b8e0d9890e0a50dbe2b1ecb70823b
Ijiekhuamen Rex <rexijie@gmail.com>
2017-06-08 02:32:46 +0100

    added a www filter class

---
+++ src/main/java/com/thelxg/data/models/components/redirectFilter.java
@@ -0,0 +1,46 @@
+package com.thelxg.data.models.components;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by rex on 08/06/2017.
+ */
+public class redirectFilter implements Filter {
+
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain chain)
+            throws IOException, ServletException {
+        if(arg0 instanceof HttpServletRequest) {
+            HttpServletRequest req = (HttpServletRequest)arg0;
+            String url = req.getRequestURL().toString();//+"?"+req.getQueryString();
+            Pattern p = Pattern.compile("(?i)(http(s?)://)www\\.");
+            Matcher m = p.matcher(url);
+            if(m.find()) {
+                //www is present -> continue
+                chain.doFilter(arg0, arg1);
+            } else {
+                StringBuilder wwwurl = new StringBuilder();
+                if(url.toLowerCase().startsWith("http://")) {
+                    wwwurl.append("http://www.").append(url.substring(7));
+                } else if(url.toLowerCase().startsWith("https://")) {
+                    wwwurl.append("https://www.").append(url.substring(8));
+                }
+                ((HttpServletResponse)arg1).sendRedirect(wwwurl.toString());
+            }
+        }
+    }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+    }
+}
