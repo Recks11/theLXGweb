c4505dd959c8b7dfa1bc9b03d29f92b6078205d1
Ijiekhuamen Rex <rexijie@gmail.com>
2017-08-02 06:55:43 +0100

    Big Update

--- src/main/java/com/thelxg/data/models/player.java
+++ src/main/java/com/thelxg/data/models/player.java
@@ -110,7 +110,7 @@ public class player {
     }
 
     public String getFullName() {
-        return fullName;
+        return getFirstName() + " " + getLastName();
     }
 
     public void setFullName(String fullName) {
